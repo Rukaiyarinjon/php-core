765946a33524a608898f139578c75aafa2e476fb
Rukaiyarinjon <info.rukaiyarinjon@gmail.com>
2024-11-13 12:54:51 +0600

    first commit

---
+++ factorial.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factorial</title>
+</head>
+<body>
+    <form method_exists="post">
+        Enter the Number: <br>
+        <input type="number" name="number" id="number">
+        <input type="submit" name="submit" value="submit"/>
+</form>
+<?php
+if($_POST){
+    $fact =1;
+    $number = $_POST['number'];
+    echo "Factorial of $number:<br><br>";
+    for($i=1; $i<= $number;$i++){
+        $fact = $fact * $i;
+    }
+    echo $fact ."<br>";
+}
+?>
+
+
+</body>
+</html>
---
+++ grade.php
@@ -0,0 +1,19 @@
+<?php
+$a=23;
+$b=10;
+$c=6;
+if($a>$b){
+    echo "This is true statement";
+    echo "<br>";
+}
+
+
+$marks=59;
+if($marks>=80){
+    echo 'grade is A+';
+}else if($marks>=70){
+    echo 'grade s A';
+}else{
+    echo 'Fail';
+}
+?>
